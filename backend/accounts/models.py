496453e110a050cbd1508ecfaca956877f48229e
wonsub.choi <whatsup@khu.ac.kr>
2020-03-05 22:26:40 +0900

    [Update] Account User Models Customize

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,11 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
 # Create your models here.
-class User(AbstractUser):
-    user_id = models.EmailField(max_length=100, unique=True)    # ID(Email 주소 형태)
-    user_name = models.CharField(max_length=100)                # 이름
-    created_at = models.DateTimeField(auto_now_add=True)        # 가입일
+class UserManager(BaseUserManager):
+    def create_user(self, **kwargs):
+        if not kwargs['user_id']:
+            raise ValueError('Users must have an user_id')
+
+        user = self.model(
+            user_id=kwargs['user_id'],
+            user_name=kwargs['user_name'],
+        )
+
+        user.set_password(kwargs['password'])
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_id, password):
+        user = self.create_user(
+            user_id,
+            password=password
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    user_id = models.EmailField(max_length=255, unique=True)
+    user_name = models.CharField(max_length=50, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = ['user_name']
 
     class Meta:
         db_table = 'account'
