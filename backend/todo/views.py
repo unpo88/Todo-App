7eb05722c0c3648730d4e516dc0b828ee9788dc9
wonsub.choi <whatsup@khu.ac.kr>
2020-03-06 20:42:03 +0900

    [Update] TODO Views Add Permission Control

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import TodoSerializer
 from .models import Todo
 
 # TodoView API
 class TodoView(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
     serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        return self.request.user.todos.all()
+ 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
